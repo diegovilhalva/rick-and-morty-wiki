b703e731250c854fdbe87d4d413a367b6c28b6b6
Diego Vilhalva <diegorocha450@yahoo.com>
2024-07-31 15:14:23 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container container-fluid px-2 py-2">
+        <h1 className="ubuntu text-center my-5 fw-bold">Rick & Morty <span className="text-primary">Wiki</span></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
