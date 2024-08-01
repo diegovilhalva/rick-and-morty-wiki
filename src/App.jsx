ac7e02e19024ada0e0d3d91d7f64e10272be8193
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-01 10:34:38 -0300

    card component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useEffect, useState } from "react"
+import Cards from "./components/Cards/Cards"
+import Filters from "./components/Filters/Filters"
 
 
 
 function App() {
 
+  const [pageNumber, setPageNumber] = useState(1)
+  const [data, setData] = useState([])
+  const { info, results } = data
+  const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(api)
+      const result = await res.json()
+      if (res.ok) {
+        setData(result)
+      }
+    }
+    fetchData()
+  }, [api])
+
+
+
 
   return (
     <>
-      <div className="container container-fluid px-2 py-2">
-        <h1 className="ubuntu text-center my-5 fw-bold">Rick & Morty <span className="text-primary">Wiki</span></h1>
+      <div className="">
+        <h1 className="ubuntu text-center my-4 fw-bold">Rick & Morty <span className="text-primary">Wiki</span></h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <Filters />
+            </div>
+            <div className="col-lg-8 col-12">
+              <div className="row">
+                <Cards results={results} />
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </>
   )
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from "./Cards.module.scss"
+
+const Cards = ({ results }) => {
+    if (!results || results.length === 0) {
+        return <div>No Characters Found :/</div>;
+    }
+
+    const getBadgeClass = (status) => {
+        switch (status) {
+            case 'Alive':
+                return 'bg-success';  
+            case 'Dead':
+                return 'bg-danger';  
+            case 'Unknown':
+            default:
+                return 'bg-secondary';  
+        }
+    };
+
+    return (
+        <div className="row">
+            {results.map((result) => (
+                <div className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative h-" key={result.id}>
+                    <div className={` ${styles.cards} d-flex flex-column justify-content-center`}>
+                        <img src={result.image} alt={result.name} className={`${styles.img} img-fluid`} />
+                        <div  className={`${styles.content} content`}>
+                            <div className="fs-4 fw-bold mb-4">{result.name}</div>
+                            <div className="fs-6">Last location</div>
+                            <div className="fs-5">{result.location.name}</div>
+                        </div>
+                    </div>
+                    <div
+                        className={`${styles.badge}  position-absolute badge  ${getBadgeClass(result.status)}`}
+                       
+                    >
+                        {result.status}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Cards;
